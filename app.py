e934164e94002bf13cfce0f1d685649f7634b5ed
Cameron Braverman <bravermancj@gmail.com>
2020-05-23 16:44:48 -0400

    Templating

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', name='Cam & Lauren')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
